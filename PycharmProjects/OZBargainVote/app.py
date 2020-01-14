6cd55a61dc605073b95e541bb1f26ba50d2ffde2
qinghuawu <qinghuawu_luke@163.com>
2020-01-14 20:03:56 +0800

    Search function

--- PycharmProjects/OZBargainVote/app.py
+++ PycharmProjects/OZBargainVote/app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request, flash, redirect
 from config import DevelopmentConfig
 import redis
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig())
@@ -13,26 +13,40 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User(user_id).retrieve_user()
+    return User(user_id).retrieve_user_by_id()
 
 
 class User(UserMixin):
-    def __init__(self, user_name):
+    def __init__(self, user_id=-1):
+        self.username = ''
+        self.id = user_id
         self.password_hash = ''
-        self.id = user_name
-        self.id_seq = -1
 
-    def register(self, password):
+    def register(self, username, password):
+        self.username = username
+        if redis_cli.hget('user:index', self.username):
+            return False
+        self.id = redis_cli.incr('user:id')
+        redis_cli.hset('user:index', self.username, self.id)  # 会和hget冲突，需锁或watch
         self.set_password(password)
-        self.id_seq = redis_cli.incr('user:id')
-        redis_cli.hmset(f'user:{self.id}', {'id_seq': self.id_seq, 'password': self.password_hash})
+        redis_cli.hmset(f'user:info:{self.id}', {'name': self.username, 'password': self.password_hash})
+        return True
 
-    def retrieve_user(self):
-        user = redis_cli.hgetall(f'user:{self.id}')
-        if not user:
-            return None
+    def retrieve_user(self, username):
+        self.username = username
+        self.id = redis_cli.hget('user:index', self.username)
+        if not self.id:
+            return False
+        user = redis_cli.hgetall(f'user:info:{self.id}')
         self.password_hash = user['password']
-        self.id_seq = user['id_seq']
+        return self
+
+    def retrieve_user_by_id(self):
+        user = redis_cli.hgetall(f'user:info:{self.id}')
+        if not user:
+            return False
+        self.username = user['name']
+        self.password_hash = user['password']
         return self
 
     def set_password(self, password):
@@ -42,63 +56,152 @@ class User(UserMixin):
         return check_password_hash(self.password_hash, password)
 
 
-class Movie:
-    def __init__(self, user_id, title, author):
-        movie_id = redis_cli.incr('movie:id')
-        redis_cli.hmset('movie:' + str(movie_id), {'title': title, 'author': author})
-        redis_cli.sadd('user' + str(user_id) + ':movie', title)
-
-
-# movies = [
-#     {'title': 'My Neighbor Totoro', 'year': '1988'},
-#     {'title': 'Dead Poets Society', 'year': '1989'},
-#     {'title': 'A Perfect World', 'year': '1993'},
-#     {'title': 'Leon', 'year': '1994'},
-#     {'title': 'Mahjong', 'year': '1996'},
-#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
-#     {'title': 'King of Comedy', 'year': '1999'},
-#     {'title': 'Devils on the Doorstep', 'year': '1999'},
-#     {'title': 'WALL-E', 'year': '2008'},
-#     {'title': 'The Pork of Music', 'year': '2012'},
-# ]
-
-name = 'wqh'
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        movies = redis_cli.smembers('user3:movie')
-        return render_template('index.html', name=name, movies=movies)
+    top_discount = redis_cli.zrange('discount:highest', 0, 41, desc=True, withscores=True)
+    products = []
+    for product_id, score in top_discount:
+        product = redis_cli.hgetall(f'item:info:{product_id}')
+        product['discount'] = str(round(float(product['discount']) * 100)) + '%'
+        products.append(product)
+    products = [products[i:i+3] for i in range(0, len(products), 3)]
+    return render_template('index.html', products=products)
 
-    if not current_user.is_authenticated:
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    username = request.form['username']
+    password = request.form['password']
+    if not User().register(username, password):
+        flash('Please try another username.')
+        return redirect(url_for('signup'))
+    else:
+        flash('Success! Please login.')
         return redirect(url_for('login'))
-    title = request.form.get('title')
-    author = request.form.get('author')
-    if len(title) == 1:
-        flash('Invalid input')
-        return redirect(url_for('index'))
-    Movie(3, title, author)
-    flash('Item added')
-    return redirect(url_for('index'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html', name=name)
-    user_name = request.form['username']
+        return render_template('login.html')
+    username = request.form['username']
     password = request.form['password']
-    user = User(user_name).retrieve_user()
+    user = User().retrieve_user(username)
     if user and user.validate_password(password):
         login_user(user)
+        print(current_user, current_user.id)
         flash('Login success.')
         return redirect(url_for('index'))
     flash('Invalid username or password')
     return redirect(url_for('login'))
 
 
-@app.route('/movie/delete/<movie_id>', methods=['POST'])
-def delete(movie_id):
-    flash(f'{movie_id} deleted')
+@app.route('/like/<int:user_id>/<int:item_id>')
+def like(user_id, item_id):
+    if not redis_cli.sadd(f'user:like:{user_id}', item_id):
+        flash('You already liked.')
+    else:
+        redis_cli.zincrby('like:count', 1, item_id)
+    return redirect(request.referrer)
+
+
+@app.route('/star/<int:user_id>/<int:item_id>')
+def star(user_id, item_id):
+    if not redis_cli.sadd(f'user:star:{user_id}', item_id):
+        flash('You already starred.')
+    return redirect(request.referrer)
+
+
+@app.route('/like_board')
+def like_board():
+    top_products = redis_cli.zrange('like:count', 0, 9, desc=True, withscores=True)
+    product_info = []
+    for product_id, score in top_products:
+        product = redis_cli.hgetall(f'item:info:{product_id}')
+        product['like_count'] = int(score)
+        product_info.append(product)
+    return render_template('like_board.html', product_info=product_info)
+
+
+@app.route('/discount_board')
+def discount_board():
+    lowest_in_history = []
+    item_id_list = redis_cli.srandmember('price:lowest_history', 54)
+    for item_id in item_id_list:
+        product = redis_cli.hgetall(f'item:info:{item_id}')
+        product['discount'] = str(round(float(product['discount']) * 100)) + '%'
+        lowest_in_history.append(product)
+    lowest_in_history = [lowest_in_history[i:i + 3] for i in range(0, len(lowest_in_history), 3)]
+    return render_template('discount_board.html', products=lowest_in_history)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logout successfully.')
     return redirect(url_for('index'))
+
+
+@app.route('/user_space')
+def user_space():
+    return render_template('user_space.html')
+
+
+@app.route('/my_like')
+def my_like():
+    like_product_ids = redis_cli.smembers(f'user:like:{current_user.id}')
+    products = []
+    for item_id in like_product_ids:
+        product = redis_cli.hgetall(f'item:info:{item_id}')
+        product['discount'] = str(round(float(product['discount']) * 100)) + '%'
+        products.append(product)
+    return render_template('my_like.html', products=products)
+
+
+@app.route('/cancel_like/<int:item_id>')
+def cancel_like(item_id):
+    redis_cli.srem(f'user:like:{current_user.id}', item_id)
+    redis_cli.zincrby('like:count', -1, item_id)
+    return redirect(url_for('my_like'))
+
+
+@app.route('/my_collection')
+def my_collection():
+    star_product_ids = redis_cli.smembers(f'user:star:{current_user.id}')
+    products = []
+    for item_id in star_product_ids:
+        product = redis_cli.hgetall(f'item:info:{item_id}')
+        product['discount'] = str(round(float(product['discount']) * 100)) + '%'
+        products.append(product)
+    return render_template('my_collection.html', products=products)
+
+
+@app.route('/cancel_star/<int:item_id>')
+def cancel_star(item_id):
+    redis_cli.srem(f'user:star:{current_user.id}', item_id)
+    return redirect(url_for('my_collection'))
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    search_content = str(request.form['search_content']).lower()
+    search_item_ids = redis_cli.smembers(f'{search_content}:items')
+    search_res = []
+    for item_id in search_item_ids:
+        product = redis_cli.hgetall(f'item:info:{item_id}')
+        product['discount'] = str(round(float(product['discount']) * 100)) + '%'
+        search_res.append(product)
+    num_items = len(search_res)
+    search_res = [search_res[i:i+3] for i in range(0, len(search_res), 3)]
+    return render_template('search.html', search_res=search_res, num=num_items)
+
+
+@app.route('/auto-completion', methods=['GET', 'POST'])
+def auto_completion():
+    search_content = str(request.form['search_content']).lower()
+    search_res = redis_cli.zrangebylex('brands:', '['+search_content, '('+search_content+'{')
+    print(search_res)
+    return render_template('search.html', search_res=search_res)
---
+++ PycharmProjects/OZBargainVote/chemist_spyder.py
@@ -0,0 +1,113 @@
+import aiohttp
+import asyncio
+import re
+import time
+import aioredis
+from bs4 import BeautifulSoup
+import heapq
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+async def open_url(url, session, headers):
+    try:
+        async with session.get(url, headers=headers) as resp:
+            data = await resp.text()
+            soup = BeautifulSoup(data, 'lxml')
+            return soup
+    except:
+        pass
+
+
+async def get_product(soups, db_pool, heap, lowest_in_history, brands):
+    category = soups[0].find('span', attrs={'id': 'category_title_span'}).text.strip()
+    for soup in soups:
+        L = soup.find('div', attrs={'class': 'product-list-container'}).find_all('td')
+        while not L[-1].text:
+            L = L[:-1]
+        for item in L:
+            mapping = {'title_chinese': item.find('a').get('title').strip(),
+                       'title': item.find('img').get('alt').strip(),
+                       'price': float(item.find('span').text.strip()[1:]),
+                       'url': 'https://www.chemistwarehouse.com.au' + item.find('a').get('href'),
+                       'image': item.find('img').get('src'),
+                       'category': category}
+            item_index = await db_pool.hget('item:index', mapping['title'])
+            if not item_index:
+                item_index = await db_pool.incr('item:id')
+                await db_pool.hset('item:index', mapping['title'], item_index)
+            mapping['item_id'] = int(item_index)
+            mapping['original_price'] = float(mapping['price'] + (
+                float(item.find('span', attrs={'class': 'Save'}).text.strip()[4:])
+                if item.find('span', attrs={'class': 'Save'}) else 0))
+
+            lowest_price_history = float(await db_pool.hget(f'item:info:{mapping["item_id"]}', 'lowest_price') \
+                                   or mapping['price'])
+            mapping['lowest_price'] = min(lowest_price_history, mapping['price'])
+            if mapping['lowest_price'] == mapping['price']:
+                lowest_in_history.append(mapping['item_id'])
+            mapping['discount'] = float((mapping['original_price'] - mapping['price']) / mapping['original_price'])
+            await db_pool.hmset_dict(f'item:info:{mapping["item_id"]}', mapping)
+            brand = mapping['title'].split()[0].lower()
+            brands.add(brand)
+            await db_pool.sadd(f'{brand}:items', mapping['item_id'])
+            heapq.heappush(heap, (mapping['discount'], mapping['item_id']))
+            if len(heap) > 150:
+                heapq.heappop(heap)
+
+
+async def item_by_category(category, session, headers, db_pool, heap, lowest_in_history, brands):
+    url = 'https://www.chemistwarehouse.hk/Shop-OnLine/' + category + '?size=120'
+    print(category, 'started at', time.strftime('%X'))
+    main_soup = await open_url(url, session, headers)
+    soups = [main_soup]
+    # print(category)
+    if main_soup.find('a', attrs={'class': 'last-page'}):
+        last = int(re.search(r'\d+$', main_soup.find('a', attrs={'class': 'last-page'}).get('href')).group())
+        for page in range(2, last + 1):
+            await asyncio.sleep(0.1)
+            soup = await open_url(url + '&page=' + str(page), session, headers)
+            # print(category)
+            if soup:
+                soups.append(soup)
+    await get_product(soups, db_pool, heap, lowest_in_history, brands)
+    print(category, 'done!', time.strftime('%X'))
+
+
+async def init_spyder():
+    try:
+        while True:
+            async with aiohttp.ClientSession() as session:
+                headers = {
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept-Language': 'zh-CN,zh;q=0.9',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Connection': 'keep-alive'
+                }
+                categories = ['256/health', '257/beauty', '258/medicines', '259/personal-care', '260/medical-aids']
+                redis_pool = await aioredis.create_redis_pool('redis://localhost')
+                heap = []
+                lowest_in_history = []
+                brands = set()
+                todo = [item_by_category(x, session, headers, redis_pool, heap, lowest_in_history, brands) for x in categories]
+                await asyncio.gather(*todo)
+                tr = redis_pool.multi_exec()
+                tr.delete('discount:highest')
+                tr.zadd('discount:highest', *[item for pair in heapq.nsmallest(150, heap) for item in pair])
+                tr.delete('price:lowest_history')
+                tr.sadd('price:lowest_history', *lowest_in_history)
+                tr.delete('brands:')
+                tr.zadd('brands:', *[item for brand in brands for item in [0, brand]])
+                await tr.execute()
+                sleep_time = 3.5 * 60 * 60
+            await asyncio.sleep(sleep_time)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        if session:
+            await session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(init_spyder())
